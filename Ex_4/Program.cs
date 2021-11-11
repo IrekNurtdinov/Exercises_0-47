a02d7834a14820a62c884d4f6d7eeaf46590bcb6
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-11 22:36:36 +0300

    fourth exercise is solved

--- Ex_4/Program.cs
+++ Ex_4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Найти максимальное из трех чисел
+
+int FindMax(int a, int b, int c)
+{
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+return max;
+}
+Console.WriteLine("Insert first number:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert second number:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert third number:");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Maximum number is {FindMax(a,b,c)}");
+
