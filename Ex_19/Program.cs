2fe689584435a360bed0f6a73a2b789c36d31580
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-20 19:11:49 +0300

    19 ex solved

---
+++ Ex_19/Program.cs
@@ -0,0 +1,19 @@
+﻿// 19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+// Status
+
+
+int Quarter(int x, int y)
+{
+    int quarterNum = 1;
+    if (x < 0 && y > 0 ) quarterNum = 2;
+    else if (x < 0 && y < 0) quarterNum = 3;
+    else if (x > 0 && y < 0) quarterNum = 4;
+    return quarterNum;
+}
+ 
+System.Console.WriteLine($"Точка находится в плоскости №{Quarter(5,6)}");
+System.Console.WriteLine($"Точка находится в плоскости №{Quarter(-5,6)}");
+System.Console.WriteLine($"Точка находится в плоскости №{Quarter(-5,-6)}");
+System.Console.WriteLine($"Точка находится в плоскости №{Quarter(5,-6)}");
+
