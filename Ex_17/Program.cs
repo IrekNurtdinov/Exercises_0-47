8e1dc8d282501d591aa59f806e8ae7282fcc18d8
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-20 17:42:22 +0300

    ex 17 solved

---
+++ Ex_17/Program.cs
@@ -0,0 +1,14 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+// Status: решено
+
+int a = 3;
+int b = 10;
+
+void SqrCheck(int d, int c)
+{
+    if (d == c * c) System.Console.WriteLine($"Число {d} является квадратом числа {c}");
+    else if (c == d * d) System.Console.WriteLine($"Число {c} является квадратом числа {d}");
+    else System.Console.WriteLine("Мы совершенно посторонние друг другу числа...");
+}
+SqrCheck(a, b);
