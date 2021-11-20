4ca93a8d9c13bb7d6f70b2d207ac4ed40c384896
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-20 19:45:26 +0300

    ex 21 solved

---
+++ Ex_21/Program.cs
@@ -0,0 +1,14 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+
+// Status: решено
+
+int a = 28582;
+int b = 32155;
+
+bool Palindrom(int num)
+{
+    return (num / 10000 == num % 10) && (num/1000%10 == num/10%10);
+}
+
+System.Console.WriteLine($"Является ли число {a} палиндромом? Ответ: {Palindrom(a)}");
+System.Console.WriteLine($"Является ли число {b} палиндромом? Ответ: {Palindrom(b)}");
