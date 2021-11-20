fb6c33dc2f10fa0111cc30e08cea2901950f21da
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-20 19:33:01 +0300

    20 ex solved

---
+++ Ex_20/Program.cs
@@ -0,0 +1,17 @@
+﻿// 20. Ввести номер четверти, показать диапазоны для возможных координат
+
+
+// Status: решено
+
+void Range(int num)
+{
+    if (num == 1) System.Console.WriteLine("Дипапзоны х - от 0 до +бесконечности; у от 0 до +бесконечности"); 
+    else if (num == 2) System.Console.WriteLine("Дипапзоны х - от 0 до -бесконечности; у от 0 до +бесконечности"); 
+    else if (num == 3) System.Console.WriteLine("Дипапзоны х - от 0 до -бесконечности; у от 0 до -бесконечности"); 
+    else if (num == 4) System.Console.WriteLine("Дипапзоны х - от 0 до +бесконечности; у от 0 до -бесконечности"); 
+    else System.Console.WriteLine("Попробуйте ещё раз");
+}
+
+System.Console.WriteLine("Введите номер четверти:");
+int quarterNum = Convert.ToInt32(Console.ReadLine());
+Range(quarterNum);
