40a99d713c7479a900db7dc6a528f70e6c168742
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-20 17:52:09 +0300

    ex 18 solved

---
+++ Ex_18/Program.cs
@@ -0,0 +1,14 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+// Status: решено
+
+
+bool Method(bool x, bool y)
+{
+return !(x | y) == (!x & !y);
+}
+
+System.Console.WriteLine(Method(true, true));
+System.Console.WriteLine(Method(true, false));
+System.Console.WriteLine(Method(false, false));
+System.Console.WriteLine(Method(false, true));
