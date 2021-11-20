ff554d7851b6e998af050742c8758e0c583f1292
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-20 17:08:14 +0300

    ex 15 solved

---
+++ Ex_15/Program.cs
@@ -0,0 +1,15 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+// Status: решено
+
+
+int num = 161; //new Random().Next(-10000,10000);
+Console.WriteLine(num);
+
+bool Method(int a, int b, int c)
+{
+return (a % b == 0 && a % c == 0);
+}
+
+
+System.Console.WriteLine(Method(num, 7, 23));
