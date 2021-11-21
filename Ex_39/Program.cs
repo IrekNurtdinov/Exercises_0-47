0d15ff485b5485eafb2fb1b18598c06b2dba983d
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-21 15:41:13 +0300

    ex 39 solved

---
+++ Ex_39/Program.cs
@@ -0,0 +1,29 @@
+﻿// 39. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+// Status:
+
+int[] FillPrintArr(int N, int min, int max)  // создает N-элементный массив и заполняет случайными числами из диапазона [min, max] и выводит на экран
+{
+    int[] array = new int[N];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+    return array;
+}
+
+void PairNumProd(int[] array)  // выводит произведение пар чисел 
+{
+    int length = array.Length - 1;
+    int prod = 0;
+    for (int i = 0; i < array.Length/2; i++)
+    {
+      prod = array[i] * array[length];
+      System.Console.WriteLine(prod);
+      length--;
+    }
+}
+
+PairNumProd(FillPrintArr(11,1,5));
