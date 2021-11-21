e670414e932a3a48d87e4f9dc6c98324c86ea4a2
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-21 13:09:32 +0300

    ex 32 solved

---
+++ Ex_32/Program.cs
@@ -0,0 +1,16 @@
+﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+
+// Status: решено
+
+void FillPrintArr(int N, int min, int max)
+{
+    int[] array = new int[N];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+        System.Console.Write(array[i] + " ");
+    }
+
+}
+
+FillPrintArr(8,0,1);
