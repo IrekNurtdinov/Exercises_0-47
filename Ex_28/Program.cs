46fbd5a47e0443ac9f2bd8f743980b9776410b6f
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-21 12:06:28 +0300

    ex 28 solved

---
+++ Ex_28/Program.cs
@@ -0,0 +1,21 @@
+﻿// 28. Подсчитать сумму цифр в числе
+
+
+// Status: решено
+
+
+int SumNum(int number)
+{
+    int sum = 0;
+    for (int i = Math.Abs(number); i > 0; i /= 10)
+    {
+    sum += i % 10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine(SumNum(123));
+System.Console.WriteLine(SumNum(-2));
+System.Console.WriteLine(SumNum(123456));
+System.Console.WriteLine(SumNum(-32));
+System.Console.WriteLine(SumNum(1234));
