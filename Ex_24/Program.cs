99b7b2075d3f8be5dcadee76809d8c8a32c85bf0
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-21 11:05:52 +0300

    ex 24 solved

---
+++ Ex_24/Program.cs
@@ -0,0 +1,16 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+// Status: решено
+
+
+void Cube(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+      int a = i*i*i;
+      System.Console.WriteLine($"{i,4}^3 = {a, 4}");  
+    }
+}
+
+int num = 12;
+Cube(num);
