8ac9cfe26339f287d8bdada0e3f898a4a4ecab79
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-21 12:18:15 +0300

    ex 29 solved

---
+++ Ex_29/Program.cs
@@ -0,0 +1,21 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+// Status: решено
+
+
+int Prod(int N)
+{
+    int prod = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        prod*= i;
+    }
+    return prod;
+}
+
+System.Console.WriteLine(Prod(5));
+System.Console.WriteLine(Prod(3));
+System.Console.WriteLine(Prod(6));
+System.Console.WriteLine(Prod(8));
+
+
