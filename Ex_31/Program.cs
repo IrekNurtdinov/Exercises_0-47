0b3e3296760b988206ce027d030d495a9dab47c1
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-21 12:58:41 +0300

    ex 31 solved

---
+++ Ex_31/Program.cs
@@ -0,0 +1,20 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+
+// Status: решено
+
+int[] A = new int[8];
+int[] B = {2,45,6,4,87,98,1,5};
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       System.Console.Write($"{array[i]} "); 
+    }
+}
+
+PrintArray(A);
+System.Console.WriteLine();
+PrintArray(B);
+
+
