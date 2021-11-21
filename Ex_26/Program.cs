f1e9fa14655db8b7c9ccdbe09daffa32e9616552
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-21 11:30:55 +0300

    ex 26 solved

---
+++ Ex_26/Program.cs
@@ -0,0 +1,19 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+
+// Status: решено
+
+
+int Pow(int A, int B)
+{
+   int num = A;
+  for (int i = 1; i < B; i++)
+  {
+    num *= A;
+  }  
+  return num;
+}
+
+System.Console.WriteLine(Pow(2,3));
+System.Console.WriteLine(Pow(3,2));
+System.Console.WriteLine(Pow(4,5));
