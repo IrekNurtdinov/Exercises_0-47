279188c8111e256f8416e67d2693f8937e881e86
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-21 11:18:48 +0300

    ex 25 solved

---
+++ Ex_25/Program.cs
@@ -0,0 +1,17 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+// Status: решено
+
+
+int Sum(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int num = 6;
+System.Console.WriteLine($"Сумма чисел от 1 до {num} = {Sum(num)}");
