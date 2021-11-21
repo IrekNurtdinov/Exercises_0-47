73e806df1ece76f8a9a3fd37bfd1b1af88da63d3
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-21 11:53:49 +0300

    ex 27 solved

---
+++ Ex_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// 27. Определить количество цифр в числе
+
+// Status: решено
+
+int DigitQuant(int number)
+{
+    int count = 1;
+    for (int i = number; i/10 > 0; i/=10)
+    {
+      count++;  
+    }
+    return count;
+}
+
+System.Console.WriteLine(DigitQuant(6548));
+System.Console.WriteLine(DigitQuant(1));
+System.Console.WriteLine(DigitQuant(64));
+System.Console.WriteLine(DigitQuant(123464));
