45efdaa0ea509dd72e9a81d89da1a5c5c52aa6b4
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-21 10:59:00 +0300

    ex 23 solved

---
+++ Ex_23/Program.cs
@@ -0,0 +1,17 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N
+
+// Status: решено
+
+
+void Square(int N)
+{
+for (int i = 1; i <= N; i++)
+{
+    int a = i*i;
+    System.Console.WriteLine($"{i,4}^2 = {a, 5}"); 
+}
+
+}
+
+int num = 12;
+Square(num);
