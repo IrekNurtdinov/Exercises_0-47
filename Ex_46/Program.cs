02b6c363b8a5c89e5d4481274e46c3a1f345560e
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-22 21:12:41 +0300

    ex 46 solving

---
+++ Ex_46/Program.cs
@@ -0,0 +1,62 @@
+﻿// 46. Написать программу масштабирования фигуры
+
+
+
+
+string a = "1(-555,0) (2,0) (2,2) (0,2)";
+// int k = 2;
+
+
+
+
+int FindIndexOf(string text, char sign)   // метод возвращает индекс (int) первого знака 'sign' (char) в строке text (string)
+{
+  int index = 0;
+    
+  for (int i = 0; i < text.Length; i++)
+  {
+    if (text[i] == sign) 
+    {
+      index = i;
+      break; 
+    } 
+  }
+  return index;
+}
+
+string DeleteChar(string text, char sign)  // метод возвращает строку string text удалив из нее все знаки char 'sign'
+{
+  string newText = String.Empty;
+  for (int i = 0; i < text.Length; i++)
+  {
+    if (text[i] != sign)
+    {
+        newText += $"{text[i]}";
+    }
+  }
+  return newText;
+}
+
+int NumBetweenSign(string text, char firstSign, char secondSign) // метод возвращает int число, которое находится между символами firstSign и secondSign в строке text                                                               
+{
+  int count = FindIndexOf(text, secondSign) - FindIndexOf(text, firstSign)-1;
+  string num = string.Empty;
+  for (int i = FindIndexOf(text, firstSign); i <= count; i++)
+  {
+      num += $"{text[i+1]}";
+  }
+  int number = Convert.ToInt32(num);
+   return number;
+}
+
+
+Console.Write(NumBetweenSign(a,'(',','));
+
+//System.Console.WriteLine(FindIndexOf(a,','));
+
+// string b = DeleteChar(a,'"');
+// string c = DeleteChar(b,'(');
+// string d = DeleteChar(c,')');
+
+// System.Console.WriteLine(d);
+
