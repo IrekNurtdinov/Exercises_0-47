f26dd48e7726e7acd923d1989097092845e84bc2
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-26 07:48:11 +0300

    ex 41 solved

--- Ex_41/Program.cs
+++ Ex_41/Program.cs
@@ -1,2 +1,18 @@
 ﻿// 41. Выяснить являются ли три числа сторонами треугольника 
 
+// Status: решено
+
+int a = 3;
+int b = 2;
+int c = 2;
+
+// если сумма любых двух сторон больше третьей
+
+
+bool IsTriangle(int a,int b,int c)
+{
+    return (a + b) > c && (b + c) > a && (c + a) > b;
+}
+
+
+System.Console.WriteLine(IsTriangle(a,b,c));
